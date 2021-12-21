d8dbcef578b406ed4cc1cbbae3c218565c71cc26
hamzasehouli <sehoulihamza@yahoo.com>
2021-12-21 12:46:41 +0100

    Last changes

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,9 +1,5 @@
 exports.catchError = async (err, req, res, next) => {
   if (!req.url.startsWith("/api/v1/")) {
-    return res.status(400).render("_errorPage", {
-      //   statusCode: err.statusCode,
-      //   status: err.status,
-      //   error: err.message,
-    });
+    return res.status(400).render("_errorPage", {});
   }
 };
