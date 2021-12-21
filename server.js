4a5bb18e2147060143c5ba6acef027a7f81057af
hamzasehouli <sehoulihamza@yahoo.com>
2021-12-21 15:12:04 +0100

    Set prerequisites

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require("path");
 const dotenv = require("dotenv");
 
 dotenv.config({ path: path.join(__dirname, ".env") });
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8000;
 
 app.listen(port, () => {
   console.log(`server is running on port ${process.env.PORT}`);
